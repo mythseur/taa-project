12c848d6edf14b63e8f3be731f6b3007b6ed8605
Guillaume Chopin <guillaume.chopin@etudiant.univ-rennes1.fr>
2016-10-07 12:57:27 +0200

    Création d'entreprise

--- src/main/webapp/app/entities/entreprise/entreprise-dialog.controller.js
+++ src/main/webapp/app/entities/entreprise/entreprise-dialog.controller.js
@@ -5,16 +5,23 @@
         .module('taaProjectApp')
         .controller('EntrepriseDialogController', EntrepriseDialogController);
 
-    EntrepriseDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Entreprise', 'DonneesEntreprise', 'Contact'];
+    EntrepriseDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Entreprise', 'DonneesEntreprise', 'Contact', 'EntrepriseDernieresDonnees'];
 
-    function EntrepriseDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Entreprise, DonneesEntreprise, Contact) {
+    function EntrepriseDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Entreprise, DonneesEntreprise, Contact, EntrepriseDernieresDonnees) {
         var vm = this;
 
         vm.entreprise = entity;
         vm.clear = clear;
         vm.save = save;
-        vm.donneesentreprises = DonneesEntreprise.query();
         vm.contacts = Contact.query();
+
+        if(vm.entreprise.id != null) {
+            EntrepriseDernieresDonnees.get(
+                {id: vm.entreprise.id},
+                function (data) {
+                    vm.donneesentreprise = data;
+                });
+        }
 
         $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
@@ -27,15 +34,22 @@
         function save() {
             vm.isSaving = true;
             if (vm.entreprise.id !== null) {
-                Entreprise.update(vm.entreprise, onSaveSuccess, onSaveError);
+                Entreprise.update(vm.entreprise, saveDonnees, onSaveError);
             } else {
-                Entreprise.save(vm.entreprise, onSaveSuccess, onSaveError);
+                Entreprise.save(vm.entreprise, saveDonnees, onSaveError);
             }
         }
 
+        function saveDonnees(result){
+            vm.donneesentreprise.datemodif = null;
+            vm.donneesentreprise.id = null;
+            vm.donneesentreprise.entreprise = result;
+            DonneesEntreprise.save(vm.donneesentreprise, onSaveSuccess, onSaveError);
+        }
+
         function onSaveSuccess(result) {
-            $scope.$emit('taaProjectApp:entrepriseUpdate', result);
-            $uibModalInstance.close(result);
+            $scope.$emit('taaProjectApp:entrepriseUpdate', result.entreprise);
+            $uibModalInstance.close(result.entreprise);
             vm.isSaving = false;
         }
 
