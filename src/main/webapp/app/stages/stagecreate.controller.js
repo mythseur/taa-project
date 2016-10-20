1a479ae3b726c0cc7ca3a94f662514b9b527d448
Guillaume Chopin <guillaume.chopin@etudiant.univ-rennes1.fr>
2016-10-20 23:06:17 +0200

    Création d'un stage.

--- src/main/webapp/app/stages/stagecreate.controller.js
+++ src/main/webapp/app/stages/stagecreate.controller.js
@@ -5,30 +5,137 @@
         .module('taaProjectApp')
         .controller('StageCreateController', StageController);
 
-    StageController.$inject = ['$scope', '$state', 'Stage', 'StageSearch'];
+    StageController.$inject = ['$scope', '$state', 'Stage', 'StageSearch', 'EtudiantSearch', 'EntrepriseSearch', 'EnseignantSearch', 'ContactSearch'];
 
-    function StageController($scope, $state, Stage, StageSearch) {
+    function StageController($scope, $state, Stage, StageSearch, EtudiantSearch, EntrepriseSearch, EnseignantSearch, ContactSearch) {
         var vm = this;
 
-        vm.stages = [];
-        vm.search = search;
-        vm.loadAll = loadAll;
 
-        loadAll();
-
-        function loadAll() {
-            Stage.query(function (result) {
-                vm.stages = result;
+        vm.selectedEtudiant = null;
+        vm.queryEtudiant = null;
+        vm.etudiants = null;
+        vm.searchEtudiant = function(){
+            EtudiantSearch.query({query: vm.queryEtudiant}, function (result) {
+                vm.etudiants = result;
+                vm.stagesEtudiant = null;
+                vm.selectedEtudiant = null;
+                if(vm.etudiants != null && vm.etudiants.length > 0){
+                    vm.selectEtudiant(vm.etudiants[0]);
+                }
             });
+        };
+        vm.selectEtudiant = function(etud){
+            vm.selectedEtudiant = etud;
+        };
+
+
+        vm.selectedEntreprise = null;
+        vm.queryEntreprise = null;
+        vm.entreprises = null;
+        vm.searchEntreprise = function(){
+            EntrepriseSearch.query({query: vm.queryEntreprise}, function (result) {
+                vm.entreprises = result;
+                vm.stagesEntreprise = null;
+                vm.selectedEntreprise = null;
+                if(vm.entreprises != null && vm.entreprises.length > 0){
+                    vm.selectEntreprise(vm.entreprises[0]);
+                }
+            });
+        };
+        vm.selectEntreprise = function(etud){
+            vm.selectedEntreprise = etud;
+        };
+
+
+        vm.selectedEncadrant = null;
+        vm.queryEncadrant = null;
+        vm.encadrants = null;
+        vm.searchEncadrant = function(){
+            ContactSearch.query({query: vm.queryEncadrant}, function (result) {
+                vm.encadrants = result;
+                vm.stagesEncadrant = null;
+                vm.selectedEncadrant = null;
+                if(vm.encadrants != null && vm.encadrants.length > 0){
+                    vm.selectEncadrant(vm.encadrants[0]);
+                }
+            });
+        };
+        vm.selectEncadrant = function(etud){
+            vm.selectedEncadrant = etud;
+        };
+
+
+
+
+        vm.selectedResponsable = null;
+        vm.queryResponsable = null;
+        vm.responsables = null;
+        vm.searchResponsable = function(){
+            ContactSearch.query({query: vm.queryResponsable}, function (result) {
+                vm.responsables = result;
+                vm.stagesResponsable = null;
+                vm.selectedResponsable = null;
+                if(vm.responsables != null && vm.responsables.length > 0){
+                    vm.selectResponsable(vm.responsables[0]);
+                }
+            });
+        };
+        vm.selectResponsable = function(etud){
+            vm.selectedResponsable = etud;
+        };
+
+
+        vm.selectedEnseignant = null;
+        vm.queryEnseignant = null;
+        vm.enseignants = null;
+        vm.searchEnseignant = function(){
+            EnseignantSearch.query({query: vm.queryEnseignant}, function (result) {
+                vm.enseignants = result;
+                vm.stagesEnseignant = null;
+                vm.selectedEnseignant = null;
+                if(vm.enseignants != null && vm.enseignants.length > 0){
+                    vm.selectEnseignant(vm.enseignants[0]);
+                }
+            });
+        };
+        vm.selectEnseignant = function(etud){
+            vm.selectedEnseignant = etud;
+        };
+
+
+        //Formulaire
+
+        vm.datePickerOpenStatus = {};
+        vm.openCalendar = openCalendar;
+
+        vm.datePickerOpenStatus.datedebut = false;
+        vm.datePickerOpenStatus.datefin = false;
+
+        function openCalendar(date) {
+            vm.datePickerOpenStatus[date] = true;
         }
 
-        function search() {
-            if (!vm.searchQuery) {
-                return vm.loadAll();
-            }
-            StageSearch.query({query: vm.searchQuery}, function (result) {
-                vm.stages = result;
-            });
+
+        vm.save = function(){
+            vm.isSaving = true;
+            vm.stage.etudiant = vm.selectedEtudiant;
+            vm.stage.entreprise = vm.selectedEntreprise;
+            vm.stage.referent = vm.selectedEnseignant;
+            vm.stage.encadrant = vm.selectedEncadrant;
+            vm.stage.responsable = vm.selectedResponsable;
+
+            Stage.save(vm.stage, onSaveSuccess, onSaveError);
+        };
+
+        function onSaveSuccess(result) {
+            $scope.$emit('taaProjectApp:stageUpdate', result);
+            $uibModalInstance.close(result);
+            vm.isSaving = false;
         }
+
+        function onSaveError() {
+            vm.isSaving = false;
+        }
+
     }
 })();
--- src/main/webapp/app/stages/stages.state.js
+++ src/main/webapp/app/stages/stages.state.js
@@ -44,6 +44,134 @@
                     }
                 },
                 resolve: {}
+            })
+            .state('etudiant.new.stage', {
+                parent: 'stageCreate',
+                url: '/newEtudiant',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                                       templateUrl: 'app/entities/etudiant/etudiant-dialog.html',
+                                       controller: 'EtudiantDialogController',
+                                       controllerAs: 'vm',
+                                       backdrop: 'static',
+                                       size: 'lg',
+                                       resolve: {
+                                           entity: function () {
+                                               return {
+                                                   iNe: null,
+                                                   nom: null,
+                                                   prenom: null,
+                                                   sexe: null,
+                                                   id: null
+                                               };
+                                           }
+                                       }
+                                   }).result.then(function () {
+                        $state.go('stageCreate', null, {reload: 'stageCreate'});
+                    }, function () {
+                        $state.go('stageCreate');
+                    });
+                }]
+            })
+            .state('entreprise.new.stage', {
+                parent: 'stageCreate',
+                url: '/newEntreprise',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                                       templateUrl: 'app/entities/entreprise/entreprise-dialog.html',
+                                       controller: 'EntrepriseDialogController',
+                                       controllerAs: 'vm',
+                                       backdrop: 'static',
+                                       size: 'lg',
+                                       resolve: {
+                                           entity: function () {
+                                               return {
+                                                   nom: null,
+                                                   siret: null,
+                                                   effectif: null,
+                                                   id: null
+                                               };
+                                           }
+                                       }
+                                   }).result.then(function () {
+                        $state.go('stageCreate', null, {reload: 'stageCreate'});
+                    }, function () {
+                        $state.go('stageCreate');
+                    });
+                }]
+            })
+            .state('enseignant.new.stage', {
+                parent: 'stageCreate',
+                url: '/newEnseignant',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                                       templateUrl: 'app/entities/enseignant/enseignant-dialog.html',
+                                       controller: 'EnseignantDialogController',
+                                       controllerAs: 'vm',
+                                       backdrop: 'static',
+                                       size: 'lg',
+                                       resolve: {
+                                           entity: function () {
+                                               return {
+                                                   sesame: null,
+                                                   nom: null,
+                                                   prenom: null,
+                                                   sexe: null,
+                                                   adresse: null,
+                                                   telpro: null,
+                                                   actif: null,
+                                                   id: null
+                                               };
+                                           }
+                                       }
+                                   }).result.then(function () {
+                        $state.go('stageCreate', null, {reload: 'stageCreate'});
+                    }, function () {
+                        $state.go('stageCreate');
+                    });
+                }]
+            })
+            .state('contact.new.stage', {
+                parent: 'stageCreate',
+                url: '/newContact',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                                       templateUrl: 'app/entities/contact/contact-dialog.html',
+                                       controller: 'ContactDialogController',
+                                       controllerAs: 'vm',
+                                       backdrop: 'static',
+                                       size: 'lg',
+                                       resolve: {
+                                           entity: function () {
+                                               return {
+                                                   nom: null,
+                                                   prenom: null,
+                                                   role: null,
+                                                   tel: null,
+                                                   mail: null,
+                                                   commentaire: null,
+                                                   id: null
+                                               };
+                                           }
+                                       }
+                                   }).result.then(function () {
+                        $state.go('stageCreate', null, {reload: 'stageCreate'});
+                    }, function () {
+                        $state.go('stageCreate');
+                    });
+                }]
             });
     }
 })();
